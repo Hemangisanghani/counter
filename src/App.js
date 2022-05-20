f8efbf0d8f3bd35e369b03e0aa5b617b09f7ba07
Hemangi <hemangisanghani7@gmail.com>
2022-05-20 14:33:13 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {actions} from './store/index'
+
 
 function App() {
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment())
+  }
+
+  const decrement = () => {
+    dispatch(actions.decrement())
+  }
+
+  const add = () => {
+    dispatch(actions.add(10))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: '50%', marginTop: '300px' }}>
+      <h1 >Counter APP</h1>
+      <h2 style={{ marginLeft: '100px' }}>{counter}</h2>
+      <button onClick={increment} style={{ padding: '10px' }}>Increment</button>
+      <button onClick={decrement} style={{ marginLeft: '30px', padding: '10px' }}>Decrement</button>
+      <button onClick={add} style={{ marginLeft: '30px', padding: '10px' }}>Add Values</button>
+
     </div>
   );
 }
